aa3f9e48bd2c6a36130fc412e2babf3b8d1d9841
Mark A. Nicolosi <mark@nicolosi.me>
2011-08-29 23:03:28 -0500

    Input handler works with touch screen

--- src/com/manico/android_jetpack/InputHandler.java
+++ src/com/manico/android_jetpack/InputHandler.java
@@ -2,27 +2,45 @@ package com.manico.android_jetpack;
 
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
 
 import java.util.HashMap;
 
-public class InputHandler implements OnKeyListener
+public class InputHandler implements OnKeyListener, OnTouchListener
 {
     boolean isLeftPressed  = false;
     boolean isRightPressed = false;
 
     public InputHandler(View view)
     {
-        Log.d("AndroidJetpack", "InputHandler()");
-
         view.setOnKeyListener(this);
+        view.setOnTouchListener(this);
         view.setFocusableInTouchMode(true);
         view.requestFocus();
     }
 
     private HashMap<Integer, Boolean> keyboard_state = new HashMap<Integer, Boolean>();
 
+    // FIXME: Return whether key was handled
+    @Override
+    public boolean onTouch(View v, MotionEvent event)
+    {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            int keycode = (int)event.getX() < (v.getWidth() / 2) ?
+                        KeyEvent.KEYCODE_DPAD_LEFT : KeyEvent.KEYCODE_DPAD_RIGHT;
+            keyboard_state.put(keycode, true);
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            keyboard_state.put(KeyEvent.KEYCODE_DPAD_LEFT, false);
+            keyboard_state.put(KeyEvent.KEYCODE_DPAD_RIGHT, false);
+        }
+
+        return true;
+    }
+
+    // FIXME: Return whether key was handled
     @Override
     public boolean onKey(View v, int keycode, KeyEvent event)
     {
